7098231e71901f893a99bc40380b3fee3d0ad5cd
David THENON <sveetch@gmail.com>
2020-09-27 03:47:23 +0200

    Add test utils with html parsers

--- {{cookiecutter.package_name}}/tests/030_views/031_blog.py
+++ {{cookiecutter.package_name}}/tests/030_views/031_blog.py
@@ -1,10 +1,16 @@
+from tests.utils import html_element, html_pyquery
+
 from {{ cookiecutter.app_name }}.factories import BlogFactory
 
 
-def test_blog_index(db, client):
+def test_blog_index_empty(db, client):
     """
-    Blog index view should list every blog
+    Without any existing blog, index view should just return the empty text.
     """
     response = client.get("/")
 
     assert response.status_code == 200
+
+    dom = html_pyquery(response)
+
+    assert "No blogs yet." == dom.find("li").text()
